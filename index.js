0e17531b4b4cdae8ebc2565766f5821f644b56ee
Luca Iacob <luca.iacob.d@gmail.com>
2020-09-08 12:52:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+var date = "October 18th"
+console.log('My birthday is ' + date);
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return (`Happpy holidays, ${name}!`)
+}
+
+var name = 'you'
